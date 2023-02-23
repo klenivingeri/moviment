ff923ea7eaf27f863646d8d9bde545873a79c16e
ERICK P A KLENIVING <libertti@live.com>
2023-02-23 10:34:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<html>
+<header>
+<link rel="stylesheet" href="./style.css">
+</header>
+<div id="fundo">
+  <div id="tela">
+    <div class="box" id="box" > </div>
+  </div>
+</div>
+<script src="./code.js"></script>
+</html>
